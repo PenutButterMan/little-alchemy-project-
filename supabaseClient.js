3f8812ba5f3cb72fa420d2c9176bde0f0a154dc8
PeanutButterMan <95386450+PenutButterMan@users.noreply.github.com>
2026-04-06 13:38:43 -0500

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://supabase.com/dashboard/project/esgqyiiwjnuaoqdcksxs";
+const SUPABASE_KEY = "sb_secret_zQWGMfpJqRqRPoBasun1jQ_GhrYPvih";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
