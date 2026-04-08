6a27d7247cf85a16ddfb056ccf80bfce881e5be3
PeanutButterMan <95386450+PenutButterMan@users.noreply.github.com>
2026-04-08 13:22:00 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://supabase.com/dashboard/project/esgqyiiwjnuaoqdcksxs";
-const SUPABASE_KEY = "sb_secret_zQWGMfpJqRqRPoBasun1jQ_GhrYPvih";
+const SUPABASE_KEY = "sb_publishable_ASL7y8N3KW6QKA7Z25m1rA_jmO-sYnr";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
