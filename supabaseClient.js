198d9bf0059604f3ecef7784b1b38c281e258350
PeanutButterMan <95386450+PenutButterMan@users.noreply.github.com>
2026-04-10 11:10:53 -0500

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,3 +1,4 @@
+"""Connects The Githup Project And Supabase Projects Together"""
 const SUPABASE_URL = "https://supabase.com/dashboard/project/esgqyiiwjnuaoqdcksxs";
 const SUPABASE_KEY = "sb_publishable_ASL7y8N3KW6QKA7Z25m1rA_jmO-sYnr";
 
