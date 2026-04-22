55d5e1f698ad07254b32c861c37e4e8f88035730
PeanutButterMan <95386450+PenutButterMan@users.noreply.github.com>
2026-04-22 13:41:27 -0500

    Update Supabase URL and key in supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,6 @@
 """Connects The Githup Project And Supabase Projects Together"""
-const SUPABASE_URL = "https://supabase.com/dashboard/project/esgqyiiwjnuaoqdcksxs";
-const SUPABASE_KEY = "sb_publishable_ASL7y8N3KW6QKA7Z25m1rA_jmO-sYnr";
+const SUPABASE_URL = "https://esgqyiiwjnuaoqdcksxs.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZ3F5aWl3am51YW9xZGNrc3hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4OTM5MjMsImV4cCI6MjA5MDQ2OTkyM30.qVXXrPtm6s8VgVdliGByjKmZsAliBf6soQ1zj74B-hA";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// connect to Supabase
+const supabase = window.supabase.createClient(https://esgqyiiwjnuaoqdcksxs.supabase.co, sb_publishable_ASL7y8N3KW6QKA7Z25m1rA_jmO-sYnr);
